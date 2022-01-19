c07d3595685d5d13fdbb7f8c64704ce9ec4a3ad7
zhuwenqi <wenqi.zhu@qihuishou.com>
2022-01-19 14:59:57 +0800

    1

---
+++ LoginModule/Classes/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  AccountModuleZhu
+//
+//  Created by ahs on 2022/1/18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "登录页面"
+        self.view.backgroundColor = .white
+    }
+
+}
---
+++ LoginModule/Classes/Target-Account.swift
@@ -0,0 +1,14 @@
+//
+//  Target-Account.swift
+//  AccountModuleZhu
+//
+//  Created by ahs on 2022/1/18.
+//
+
+import UIKit
+
+class Target_Account: NSObject {
+   public func nativeLoginViewController() -> UIViewController {
+       return LoginViewController()
+    }
+}
