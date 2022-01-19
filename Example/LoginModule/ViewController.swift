55add6209e8ade2c89b96494d173e5110a6a4830
zhuwenqi <wenqi.zhu@qihuishou.com>
2022-01-19 12:56:00 +0800

    Initial commit

---
+++ Example/LoginModule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoginModule
+//
+//  Created by zhuwenqi on 01/19/2022.
+//  Copyright (c) 2022 zhuwenqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
